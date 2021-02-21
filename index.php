da362ddd2e55a756c150821db5b9d477cd5d33f5
aloproducao <43450424+aloproducao@users.noreply.github.com>
2021-02-21 15:09:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -19,9 +19,3 @@ try {
     ErrorHandler::handle($e);
 }
 
-if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
-    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header('HTTP/1.1 301 Moved Permanently');
-    header('Location: ' . $location);
-    exit;
-}
